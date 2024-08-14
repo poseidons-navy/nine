75c53e5373873c424fc06f9f444d9ed67220e908
romannjoroge <roman.njoroge@njuguna.com>
2024-08-14 16:45:03 +0300

    feat: Removing IPFS node

--- nine-node/src/ipfs/store-request.ts
+++ nine-node/src/ipfs/store-request.ts
@@ -1,13 +1,14 @@
 import { dagJson } from "@helia/dag-json";
-import node from "./client";
 import { CreateRequestParams } from "../types";
 import { CID, Version } from 'multiformats/cid';
+import { createHelia } from "helia";
 /**
  * storeRequest - Function to store rquest details in IPFS.
  * @param args The request creation parameters.
  */
 export async function storeRequest(args: CreateRequestParams): Promise<CID<unknown, number, number, Version>> {
     try {
+        const node = await createHelia();
         const d = dagJson(node);
         const address = await d.add(args);
         return address;
