7f12359122fe7954242243c29e988bcb82e27aa6
romannjoroge <roman.njoroge@njuguna.com>
2024-09-03 19:51:03 +0300

    refactor: Deleting other db packages

--- payment_indexer/packages/db/schema/index.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, text, timestamp, integer } from "drizzle-orm/pg-core";
-export const cidEvents = pgTable("payments", {
-    id: text("id").notNull().primaryKey(),
-    cid: text("cid").notNull(),
-    amount: integer("amount").notNull(),
-    timestamp: timestamp("timestamp").notNull(),
-    payer_address: text("payer_address").notNull(),
-    payee_address: text("payee_address").notNull(),
-});
--- storage_indexer/packages/db/schema/index.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
-export const cidEvents = pgTable("cid", {
-    id: text("id").notNull().primaryKey(),
-    cid: text("cid").notNull(),
-    timestamp: timestamp("timestamp").notNull(),
-    payer_address: text("payer_adrress").notNull(),
-});
