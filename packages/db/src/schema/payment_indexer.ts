4a714cf31debf8ea2ba4574aada5085b37bef989
romannjoroge <roman.njoroge@njuguna.com>
2024-09-03 19:49:21 +0300

    feat: Adding schema from payment and storage indexers

---
+++ packages/db/src/schema/payment_indexer.ts
@@ -0,0 +1,9 @@
+import { pgTable, text, timestamp, integer } from "drizzle-orm/pg-core";
+export const paymentEvents = pgTable("payments", {
+    id: text("id").notNull().primaryKey(),
+    cid: text("cid").notNull(),
+    amount: integer("amount").notNull(),
+    timestamp: timestamp("timestamp").notNull(),
+    payer_address: text("payer_address").notNull(),
+    payee_address: text("payee_address").notNull(),
+});
