f25e3c9737f0bf7a58a29e29daae2dc6d26893bd
Roman Njoroge <roman.njoroge@njuguna.com>
2024-09-15 11:28:00 +0300

    feat: Adding clerk_id to schema

--- packages/db/src/schema/users.ts
+++ packages/db/src/schema/users.ts
@@ -2,6 +2,7 @@ import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const users = pgTable("Users", {
     id: serial("id").primaryKey(),
+    clerkID: text("clerk_id").notNull().unique(),
     address: text("address").notNull(),
     expoToken: text("expoToken").notNull()
 })
