5ae5a11220ebdc6d202e517d6250104e0915a331
Roman Njoroge <roman.njoroge@njuguna.com>
2024-09-15 11:27:40 +0300

    fix: Using the correct schema file path

--- packages/db/src/drizzle.config.ts
+++ packages/db/src/drizzle.config.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export default defineConfig({
     dialect: 'postgresql',
-    schema: "./schema/index.ts",
+    schema: "./src/schema/index.ts",
     out: "./migrations",
     dbCredentials: {
         url: process.env.NEON_CONN_STRING || "postgresql://nine_owner:Ez2yitx9jlbT@ep-restless-poetry-a2957ipe.eu-central-1.aws.neon.tech/nine?sslmode=require"
